4f4e36098f938c84c81d8ad3af0c674eb2d82457
Franciscols2011 <franciscols2011@gmail.com>
2022-07-15 14:21:37 -0300

    Se modifico el @Transactional que no retornaba nada, tambien se borro codigo que no cumple ninguna funcion.

--- Back-reto/src/main/java/com/sofka/Software/services/ListTaskService.java
+++ Back-reto/src/main/java/com/sofka/Software/services/ListTaskService.java
@@ -2,7 +2,7 @@ package com.sofka.Software.services;
 
 import com.sofka.Software.models.ListTaskModel;
 import com.sofka.Software.repositories.ListTaskRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -15,42 +15,25 @@ public class ListTaskService {
         return listTaskRepository.findAll();
     }
 
-    /**
-     * Crea las subTareas en la lisa principal
-     * @param listTask
-     * @return
-     */
+
     public ListTaskModel createListTask(ListTaskModel listTask){
         listTask.setCompleted(false);
         return listTaskRepository.save(listTask);
     }
 
-    /**
-     * metodo eliminar subtarea
-     * @param id
-     */
+
     public void deleteListTask(Long id){
         listTaskRepository.delete(get(id));
     }
-    /**
-     * verificar si el id esta entonces lo retorne , si no retorne error
-     * @param id
-     * @return
-     */
+
     public ListTaskModel get(Long id){
         return listTaskRepository.findById(id).orElseThrow();
     }
 
-    /**
-     * metodo actualizar subtarea, me permite actualizar la lista
-     * @param id
-     * @param listTask
-     * @return
-     */
+
     @Transactional
     public ListTaskModel updateListTask(Long id, ListTaskModel listTask) {
-        listTask.setId(id);
-        listTaskRepository.save(listTask);
-        return listTask;
+        listTask.setId(get(id).getId());
+        return listTaskRepository.save(listTask);
     }
-//}
+}
