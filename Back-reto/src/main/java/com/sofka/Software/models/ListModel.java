42d99d43300d40758d2dbe51a5c270e0135a6812
Franciscols2011 <franciscols2011@gmail.com>
2022-07-15 13:54:59 -0300

    Se modifico el ListModel en el cual se agregaron los seter y geter

--- Back-reto/src/main/java/com/sofka/Software/models/ListModel.java
+++ Back-reto/src/main/java/com/sofka/Software/models/ListModel.java
@@ -30,7 +30,25 @@ public class ListModel {
      */
     @JsonBackReference
     private List<ListTaskModel>ListTask = new ArrayList<>();
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public List<ListTaskModel> getListTask(){
+        return ListTask;
+    }
+    public void setListTask(List<ListTaskModel> listTask){
+        ListTask = listTask;
+    }
 
 
-
+    
 }
